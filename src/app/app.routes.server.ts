9c0625696bf7a018c28950482b59c23adf6aaa51
davidswedi <davidswedi700@gmail.com>
2025-01-31 19:13:27 +0200

    Prender Home Page

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'products/:category',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'product/:id',
+    renderMode: RenderMode.Prerender,
+  },
 ];
